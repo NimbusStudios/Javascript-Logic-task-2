90c98c797f0f78a2d3d4fbb720b944dcb5a074e2
NimbusStudios <165320227+NimbusStudios@users.noreply.github.com>
2024-07-03 10:04:55 +0200

    Add files via upload
    Part 2 of assignment

---
+++ script2b.js
@@ -0,0 +1,51 @@
+const array = [7, 10, "Clentan", 13, 89, true, 45, false, "Jerry", "Vukona", "Reabetswe", 600];
+
+// Using a for loop and a variable to return all values that are numbers
+const numbers = [];
+for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] === "number") {
+        numbers.push(array[i]);
+    }
+}
+console.log(number)
+
+// Using a while loop and a variable to return all the values that are a string
+const strings = [];
+let j = 0;
+while (j < array.length) {
+    if (typeof array[j] === "string") {
+        strings.push(array[j]);
+    }
+    j++;
+}
+console.log(strings);
+
+//Using a do while loop and if statement to return the sum of all values in the array
+let sum = 0;
+let k = 0;
+do {
+    if (typeof array[k] === "number") {
+        sum += array[k];
+    }
+    k++;
+} while (k < array.length);
+console.log(sum)
+
+// Using a for loop and a variable to combine all the strings to form a proper greeting
+let greeting = "Hello";
+for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] === "string") {
+        greeting += ", " + array[i];
+    }
+}
+console.log(greeting);
+
+
+// Using a for loop and a new variable to remove all the values in the array
+const removedArray = [];
+for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] !== "number") {
+        removedArray.push(array[i]);
+    }
+}
+console.log(removedArray)
